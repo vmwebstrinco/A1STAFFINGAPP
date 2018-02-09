68bd62c090df588118aa6231aab7b15be30b68c2
Kalakchika Sivarasa <galaxy@kalakchikas-mbp.lan>
2018-02-09 05:25:45 -0800

    Initial Commit

---
+++ A1STAFFINGTests/A1STAFFINGTests.swift
@@ -0,0 +1,36 @@
+//
+//  A1STAFFINGTests.swift
+//  A1STAFFINGTests
+//
+//  Created by Kalakchika Sivarasa on 2018-02-09.
+//  Copyright © 2018 VMEWBS ENTERPRISE LTD. All rights reserved.
+//
+
+import XCTest
+@testable import A1STAFFING
+
+class A1STAFFINGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
