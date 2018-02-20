aa995483043118115c81dc04d1e996a950fe2147
Kalakchika Sivarasa <galaxy@dinesh.lan>
2018-02-20 04:28:42 -0800

    commit 11

--- A1STAFFING/SignViewController.swift
+++ A1STAFFING/SignViewController.swift
@@ -17,11 +17,34 @@ import UIKit
 open class SignViewController: UIViewController {
     //Mark: - struct
     struct gemp: Codable {
-        let first_name: String?
+        let title: String
+        let first_name: String
+        let middle_name: String
+        let last_name: String
+        let gender: String
+        let dob: String
+        let main_phone: String
+        let mobile: String
+        let main_email: String
+        let address: String
+        let social_insurance: String
+        let category: String
+        let kmrange: String
         
         private enum CodingKeys: String, CodingKey{
+            case title
             case first_name
-            
+            case middle_name
+            case last_name
+            case gender
+            case dob
+            case main_phone
+            case mobile
+            case main_email
+            case address
+            case social_insurance
+            case category
+            case kmrange
         }
         
     }
@@ -52,17 +75,64 @@ open class SignViewController: UIViewController {
     
     // MARK: - Life cycle methods
     override open func viewDidLoad() {
-        super.viewDidLoad()
         
         bor_label.layer.borderWidth = 0.5
         bor_label.layer.borderColor = UIColor.black.cgColor
         
         details_view.layer.borderWidth = 0.5
-        details_view.layer.borderColor = UIColor.black.cgColor       
+        details_view.layer.borderColor = UIColor.black.cgColor
         
+        let inserviceUrl = "http://a1staffing.ca/app/services.php"
+        let inurl = URL(string: inserviceUrl)
+        
+        var request = URLRequest(url: inurl!)
+        request.httpMethod = "POST"
+        var dataString = "secretWord=345gt6768"
+        dataString = dataString + "&action=getdata"
+        dataString = dataString + "&id=\(insertid)"
+        
+        let dataD = dataString.data(using: .utf8)
+        
+        let group = DispatchGroup()
+        group.enter()
+        let insertEntry = URLSession.shared.uploadTask(with:request, from: dataD, completionHandler: { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async{
+                    let alert = UIAlertController(title: "Please Try Again", message: "Looks like the connection to the server didn't work. Do you have Internet access?", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                }
+            }
+            else
+            {
+                guard let data = data else { return }
+                do {
+                    let decoder = JSONDecoder()
+                    let emp = try decoder.decode(gemp.self, from: data)
+                    var mobile : String = emp.mobile
+                    var phone : String = emp.main_phone
+                    if(mobile != ""){
+                        phone += " / " + mobile
+                    }
+                    self.lbl_name.text = emp.title+" "+emp.first_name+" "+emp.middle_name+" "+emp.last_name
+                    self.lbl_gender.text = emp.gender
+                    self.lbl_dob.text = emp.dob
+                    self.lbl_mobile.text = phone
+                    self.lbl_email.text = emp.main_email
+                    self.lbl_address.text = emp.address
+                    self.lbl_sin.text = emp.social_insurance
+                    self.lbl_category.text = emp.category
+                    self.lbl_kmrange.text = emp.kmrange
+                } catch let err {
+                    print("Err", err)
+                }
+                group.leave()
+            }
+        })
+        insertEntry.resume()
+        group.wait()
+        super.viewDidLoad()
     }
-    
-    
     
     // MARK: - Initializers
     public convenience init(signatureDelegate: EPSignatureDelegate) {
@@ -95,70 +165,61 @@ open class SignViewController: UIViewController {
     
     // MARK: - Updte signature button
     @IBAction func updatesignature(_ sender: Any) {
-        self.performSegue(withIdentifier: "sequetomain", sender: self)
-    }
-    
-    override open func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "sequetomain"{
-            
-            if let signature = signatureView.getSignatureAsImage(){
-                sig = signatureView.converAsBase64()!
-                signatureDelegate?.epSignature!(self, didSign: signature, boundingRect: signatureView.getSignatureBoundsInCanvas())
-            }
-            
-            let inserviceUrl = "http://a1staffing.ca/app/services.php"
-            let inurl = URL(string: inserviceUrl)
-            
-            var request = URLRequest(url: inurl!)
-            request.httpMethod = "POST"
-            var dataString = "secretWord=12345b"
-            dataString = dataString + "&action=updatesign"
-            dataString = dataString + "&stringnature=\(sig)"
-            dataString = dataString + "&id=\(insertid)"
-            
-            let dataD = dataString.data(using: .utf8)
-            
-            let group = DispatchGroup()
-            group.enter()
-            
-            let insertEntry = URLSession.shared.uploadTask(with:request, from: dataD, completionHandler: { (data, response, error) in
-                if error != nil {
-                    DispatchQueue.main.async
-                        {
-                            let alert = UIAlertController(title: "Please Try Again", message: "Looks like the connection to the server didn't work. Do you have Internet access?", preferredStyle: .alert)
-                            alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                            self.present(alert, animated: true, completion: nil)
-                    }
-                }
-                else
-                {
-                    if let unwrappedData = data {
-                        
-                        let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
-                        
-                        if returnedData == "1"
-                        {
-                            DispatchQueue.main.async
-                                {
-                                    let alert = UIAlertController(title: "Success", message: "Successfully registered", preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                                    self.present(alert, animated: true, completion: nil)
-                            }
-                        }
-                        else
-                        {
-                            DispatchQueue.main.async
-                                {
-                                    let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
-                                    alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
-                                    self.present(alert, animated: true, completion: nil)
-                            }
-                        }
-                    }
-                }
-            })
-            insertEntry.resume()
+        if let signature = signatureView.getSignatureAsImage(){
+            sig = signatureView.converAsBase64()!
+            signatureDelegate?.epSignature!(self, didSign: signature, boundingRect: signatureView.getSignatureBoundsInCanvas())
         }
+        
+        let inserviceUrl = "http://a1staffing.ca/app/services.php"
+        let inurl = URL(string: inserviceUrl)
+        
+        var request = URLRequest(url: inurl!)
+        request.httpMethod = "POST"
+        var dataString = "secretWord=12345b"
+        dataString = dataString + "&action=updatesign"
+        dataString = dataString + "&stringnature=\(sig)"
+        dataString = dataString + "&id=\(insertid)"
+        
+        let dataD = dataString.data(using: .utf8)
+        
+        let insertEntry = URLSession.shared.uploadTask(with:request, from: dataD, completionHandler: { (data, response, error) in
+            if error != nil {
+                DispatchQueue.main.async
+                    {
+                        let alert = UIAlertController(title: "Please Try Again", message: "Looks like the connection to the server didn't work. Do you have Internet access?", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+                }
+            }
+            else
+            {
+                if let unwrappedData = data {
+                    
+                    let returnedData = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                    
+                    if returnedData == "1"
+                    {
+                        DispatchQueue.main.async
+                            {
+                                let alert = UIAlertController(title: "Success", message: "Successfully updated", preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { action in
+                                    self.performSegue(withIdentifier: "sequetomain", sender: self)
+                                }))
+                                self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                    else
+                    {
+                        DispatchQueue.main.async
+                            {
+                                let alert = UIAlertController(title: "Error", message: "Please try again later", preferredStyle: .alert)
+                                alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                                self.present(alert, animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        })
+        insertEntry.resume()
     }
 }
-
