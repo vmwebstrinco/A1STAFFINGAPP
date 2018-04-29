bef5922866eec38313b8c6250b4db6338714abde
Kalakchika Sivarasa <vmwebstrinco@gmail.com>
2018-04-29 17:25:50 +0530

    commit 26

--- A1STAFFING/extensions.swift
+++ A1STAFFING/extensions.swift
@@ -9,6 +9,8 @@
 import Foundation
 import UIKit
 
+private var __maxLengths = [UITextField: Int]()
+
 extension UIViewController {
     
     func showAlert(_ message: String) {
@@ -44,6 +46,33 @@ extension UITextField {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.rightView = paddingView
         self.rightViewMode = .always
+    }
+    
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    @objc func fix(textField: UITextField) {
+        let t = textField.text
+        textField.text = t?.safelyLimitedTo(length: maxLength)
+    }
+}
+
+extension String
+{
+    func safelyLimitedTo(length n: Int)->String {
+        if (self.count <= n) {
+            return self
+        }
+        return String( Array(self).prefix(upTo: n) )
     }
 }
 
